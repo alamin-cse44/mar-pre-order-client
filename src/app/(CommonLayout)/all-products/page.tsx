e92b557169d5b5a5017263a1e622e09b317a822a
alamin-cse44 <alamincse44@gmail.com>
2025-09-24 20:11:18 +0600

    Refactor: card items

--- src/app/(CommonLayout)/all-products/page.tsx
+++ src/app/(CommonLayout)/all-products/page.tsx
@@ -198,7 +198,7 @@ export default function AllProducts() {
 
           {/* Products Grid */}
           <main className="md:col-span-9">
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
               {currentSlice.map((p) => (
                 <ProductCard key={p.id} product={p} />
               ))}
